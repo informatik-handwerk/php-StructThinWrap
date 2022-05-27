e11b25d4de0863647c67153fd5389c7cfac99e64
bits-of-static <huuj6ueps1@informatik-handwerk.de>
2022-05-27 13:20:33 +0200

    initial

---
+++ src/php74/v10/Inline/StructThinWrapAPI_object_public.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ihde\php74\StructThinWrap\v10\Inline;
+
+
+class StructThinWrapAPI_object_public
+    implements I_StructThinWrapAPI {
+    
+    protected static self $instance;
+    
+    /**
+     *
+     */
+    protected function __construct()  {
+    }
+    
+    /**
+     * @return static
+     */
+    public static function instance(): self  {
+        if (!isset(static::$instance))  {
+            static::$instance = new static();
+        }
+        
+        return static::$instance;
+    }
+    
+    /**
+     * @implements I_StructThinWrapAPI
+     * @inheritDoc
+     */
+    public function canWorkWith(&$struct): bool {
+        return \is_object($struct);
+    }
+    
+    /**
+     * @implements I_StructThinWrapAPI
+     * @inheritDoc
+     */
+    public function &get(&$struct, $key) {
+        return $struct->$key;
+    }
+    
+    /**
+     * @implements I_StructThinWrapAPI
+     * @inheritDoc
+     */
+    public function getDefaulted(&$struct, $key, $default = null) {
+        return $struct->$key ?? $default;
+    }
+    
+    /**
+     * @implements I_StructThinWrapAPI
+     * @inheritDoc
+     */
+    public function setValue(&$struct, $key, $value): void {
+        $struct->$key = $value;
+    }
+    
+    /**
+     * @implements I_StructThinWrapAPI
+     * @inheritDoc
+     */
+    public function setReference(&$struct, $key, &$reference): void {
+        $struct->$key =& $reference;
+    }
+    
+    /**
+     * @implements I_StructThinWrapAPI
+     * @inheritDoc
+     */
+    public function unset(&$struct, $key): void {
+        unset($struct->$key);
+    }
+    
+    /**
+     * @implements I_StructThinWrapAPI
+     * @inheritDoc
+     */
+    public function &foreach(&$struct): \Generator {
+        foreach ($struct as $key => &$reference) {
+            yield $key => $reference;
+        }
+        unset($reference);
+    }
+    
+    
+}
+
